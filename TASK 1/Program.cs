0bbefba7de8eecb4e47c81a1d92e5f0ab98ee270
LOQ <LOQ@BASEM>
2025-03-01 20:39:36 +0200

    Add project files.

---
+++ TASK 1/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace TASK_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Islam's Carpit Cleaning service");
+            Console.WriteLine("Estimate for carpet cleaning service");
+            Console.WriteLine("please enter how many small carpit ");
+            int smallCarpet = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("please enter how many large carpit ");
+            int largeCarpet = Convert.ToInt32(Console.ReadLine());
+
+
+            double priceAfterTax = ((smallCarpet * 25) + (largeCarpet * 35)) * 1.06;
+            Console.WriteLine($"Total estimate is : $ {priceAfterTax:f1}");
+            Console.WriteLine($"This estimate is Valid for 30 days");
+
+        }
+    }
+}
